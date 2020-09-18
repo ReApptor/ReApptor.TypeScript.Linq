467b5a0da3d69d1c681d2fd8c4b42ddad00f05d0
evgenyoborotistov <evgeny.oborotistov@weare.fi>
2020-09-18 11:52:14 +0300

    RSW-450 Add additional config to package/tsconfig jsons

---
+++ WeAre.Athenaeum.React.Toolkit/src/index.ts
@@ -0,0 +1,20 @@
+import Utility from "@/Utility";
+import NumberUtility from "@/NumberUtility";
+import StringUtility from "@/StringUtility";
+import BoolUtility from "@/BoolUtility";
+import HashCodeUtility from "@/HashCodeUtility";
+import { DateExtensions } from "@/Extensions/DateExtensions";
+import { StringExtensions } from "@/Extensions/StringExtensions";
+import PwaHelper from "@/Helpers/PwaHelper";
+
+
+export {
+    Utility,
+    NumberUtility,
+    StringUtility,
+    BoolUtility,
+    HashCodeUtility,
+    DateExtensions,
+    StringExtensions,
+    PwaHelper
+}
