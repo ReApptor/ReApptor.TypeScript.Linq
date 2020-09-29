df9fd0ab13b0925a0b0abb0f6b60943a0394e87c
evgenyoborotistov <evgeny.oborotistov@weare.fi>
2020-09-29 13:58:11 +0300

    RSW-450 Refactor tests

--- WeAre.Athenaeum.React.Toolkit/src/tests/Utility.test.ts
+++ WeAre.Athenaeum.React.Toolkit/src/tests/Utility.test.ts
@@ -1,15 +1,12 @@
 import Utility from "../Utility";
 
-describe("Utility tests", () => {
-    describe("round", () => {
-        test("Should round the number up", () => {
-            expect(Utility.round(1.3456, 2)).toBe(1.35);
-            expect(Utility.round(1.35, 1)).toBe(1.4);
-        });
-        
-        test("Should round the number down", () => {
-            expect(Utility.round(1.3416, 2)).toBe(1.35);
-            expect(Utility.round(1.31, 1)).toBe(1.4);
-        });
-    })
-})
+describe('roundTests', function() {
+    test('roundUp', function () {
+        const result: number = Utility.round(5.678, 2);
+        expect(result).toBe(5.68);
+    });
+    test('roundDown', function () {
+        const result: number = Utility.round(5.432, 2);
+        expect(result).toBe(5.43);
+    });
+});
