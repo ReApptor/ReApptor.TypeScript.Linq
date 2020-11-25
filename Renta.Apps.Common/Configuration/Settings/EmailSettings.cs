98765b71f2ccca08b6ae21abbc4c8dbbee7e143a
Juhani Sihvonen <juhani.sihvonen@weare.fi>
2020-11-25 12:36:38 +0200

    RSW-450 add whitelist for email and sms

--- Renta.Apps.Common/Configuration/Settings/EmailSettings.cs
+++ Renta.Apps.Common/Configuration/Settings/EmailSettings.cs
@@ -11,5 +11,7 @@ namespace Renta.Apps.Common.Configuration.Settings
         public string ApiKey { get; set; }
 
         public string EmailSender { get; set; }
+        
+        public string WhiteListedDomains { get; set; }
     }
 }
--- Renta.Apps.Common/Configuration/Settings/SmsNotificationSettings.cs
+++ Renta.Apps.Common/Configuration/Settings/SmsNotificationSettings.cs
@@ -11,5 +11,8 @@ namespace Renta.Apps.Common.Configuration.Settings
         public string Password { get; set; }
 
         public string Url { get; set; }
+        
+        public string WhiteListedNumbers { get; set; }
+
     }
 }
