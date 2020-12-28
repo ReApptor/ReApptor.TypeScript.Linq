4ef531bae1fce130e613a10b87ab995e609f93ab
Artem Nefodov <artem.nefodov@weare.fi>
2020-12-28 10:41:56 +0200

    RSW-450 constants were updated. new tagRegex was added

--- WeAre.Athenaeum.React.Toolkit/src/AthenaeumConstants.ts
+++ WeAre.Athenaeum.React.Toolkit/src/AthenaeumConstants.ts
@@ -16,5 +16,7 @@ export default class AthenaeumConstants {
 
     public static readonly markTagRegex: RegExp = /<\s*mark[^>]*>(.*?)<\s*\/\s*mark>/ig;
 
+    public static readonly smallTagRegex: RegExp = /<\s*small[^>]*>(.*?)<\s*\/\s*small>/ig;
+
     public static readonly defaultGuid: string = "00000000-0000-0000-0000-000000000000";
 }
