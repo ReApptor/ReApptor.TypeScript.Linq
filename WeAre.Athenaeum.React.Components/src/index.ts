7306e507c3b002dc44c702a6d2b5487c15ce87c7
Andrey Popov <andrey.popov@weare.fi>
2021-01-08 15:37:57 +0200

    RSW-450 New REACT components project;

--- WeAre.Athenaeum.React.Components/src/index.ts
+++ WeAre.Athenaeum.React.Components/src/index.ts
@@ -1,8 +1,6 @@
 //Alert
 import Alert, { IAlertProps } from "./Alert/Alert";
-import AlertStyles from "./Alert/Alert.module.scss";
-const alertStyles: object = AlertStyles; 
 
 export {
-    Alert, IAlertProps, alertStyles,
+    Alert, IAlertProps,
 }
