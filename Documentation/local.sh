64c96eb146fe6e9b9a1c627e0794c9e38ff8f875
Juhani Sihvonen <juhani.sihvonen@weare.fi>
2021-01-08 09:31:43 +0200

    RSW-450 improve

--- Documentation/local.sh
+++ Documentation/local.sh
@@ -1,9 +1,12 @@
 npm install -g @microsoft/api-extractor
 npm install -g @microsoft/api-documenter
 cd ../WeAre.Athenaeum.React.Toolkit/
+npm install
+npm run build
 mkdir ./etc
 api-extractor run --local
 api-documenter markdown -i ./temp/ -o ../Documentation/docs/
+rm -r ./etc
 cp ../Documentation/index.html ../Documentation/docs/index.html
 cd ../Documentation/docs/
 mv index.md README.md
