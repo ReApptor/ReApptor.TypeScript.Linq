c2095cc2173b8190e20d4c302f111d266dba2c24
Eric Askari <eric.askari@weare.fi>
2021-02-18 12:13:49 +0200

    Moving shared dependencies to peerDependencies (Testing)

--- WeAre.Athenaeum.React.Common/rollup.config.js
+++ WeAre.Athenaeum.React.Common/rollup.config.js
@@ -5,7 +5,11 @@ import pkg from "./package.json";
 
 export default {
     input: "src/index.ts",
-    external: [...Object.keys(pkg.dependencies), Object.keys(pkg.devDependencies)],
+    external: [
+        ...Object.keys(pkg.dependencies),
+        ...Object.keys(pkg.devDependencies),
+        ...Object.keys(pkg.peerDependencies)
+    ],
     output: [
         {
             file: pkg.main,
