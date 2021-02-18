c0d4f69b516520237dde0478c13ee347feb31268
Eric Askari <eric.askari@weare.fi>
2021-02-18 16:08:30 +0200

    rollup security null key check

--- WeAre.Athenaeum.React.Toolkit/rollup.config.js
+++ WeAre.Athenaeum.React.Toolkit/rollup.config.js
@@ -6,9 +6,9 @@ import pkg from "./package.json";
 export default {
     input: "src/index.ts",
     external: [
-        ...Object.keys(pkg.dependencies),
-        ...Object.keys(pkg.devDependencies),
-        ...Object.keys(pkg.peerDependencies)
+        ...Object.keys(pkg.dependencies || {}) ,
+        ...Object.keys(pkg.devDependencies || {}),
+        ...Object.keys(pkg.peerDependencies || {})
     ],        
     output: [
         {
