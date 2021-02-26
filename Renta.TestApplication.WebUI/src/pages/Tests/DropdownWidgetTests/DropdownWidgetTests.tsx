e393a9c361155c5cc22b2d7173d43812ed09ab1f
Eric Askari <eric.askari@weare.fi>
2021-02-26 08:56:40 +0200

    [skip ci] Updated test application (not working yet)

---
+++ Renta.TestApplication.WebUI/src/pages/Tests/DropdownWidgetTests/DropdownWidgetTests.tsx
@@ -0,0 +1,148 @@
+import React from "react";
+import {BaseComponent, ch} from "@weare/athenaeum-react-common";
+import { AmountListItem, Checkbox, Form, List, TwoColumns } from "@weare/athenaeum-react-components";
+import { WidgetContainer, DropdownWidget } from "@weare/athenaeum-react-components";
+
+export interface IDropdownWidgetTestsState {
+    multiSelect: boolean,
+    search: boolean,
+    favorite: boolean,
+    required: boolean,
+    expanded: boolean,
+    amountListItem: boolean
+}
+
+export default class DropdownWidgetTests extends BaseComponent<{}, IDropdownWidgetTestsState> {
+
+    state: IDropdownWidgetTestsState = {
+        multiSelect: false,
+        search: false,
+        favorite: false,
+        required: false,
+        expanded: false,
+        amountListItem: false
+    };
+    
+    private readonly _listRef: React.RefObject<List> = React.createRef();
+
+    private items: any[] = [
+        {name: "1st item", group: "1"},
+        {name: "2nd item", group: "1"},
+        {name: "3d item", group: "2"},
+        {name: "4th item", group: "2"},
+        {name: "5th item", group: "3"}
+    ];
+
+    private amountItems: any[] = [
+        {name: "1st item", amount: 0, step: 1},
+        {name: "2nd item", amount: 0, step: 1},
+        {name: "3d item", amount: 0, step: 1},
+        {name: "4th item", amount: 0, step: 1},
+        {name: "5th item", amount: 0, step: 1},
+    ];
+
+    private itemsList: any[] = [
+        {name: "0th item", group: "0"},
+        {name: "1st item", group: "1"},
+        {name: "2nd item", group: "2"},
+        {name: "3d item", group: "3"},
+        {name: "4th item", group: "4"},
+        {name: "5th item", group: "5"},
+        {name: "6th item", group: "6"},
+        {name: "7th item", group: "7"},
+        {name: "8th item", group: "8"},
+        {name: "9th item", group: "9"}
+    ];
+
+    private dropdownItems(): any[] {
+        if (this.state.amountListItem) {
+            return this.amountListItems;
+        }
+        else if (this.state.search) {
+            return this.itemsList;
+        }
+
+        return this.items;
+    }
+
+    private async setSearchAsync(search: boolean): Promise<void> {
+        await this.setState({search});
+        if (this._listRef.current) {
+            await this._listRef.current.reloadAsync();
+        }
+    }
+    
+    private get amountListItems(): AmountListItem[] {
+        return this.amountItems.map((item, index) => {
+            const amountListItem: AmountListItem = new AmountListItem();
+            amountListItem.step = item.step;
+            amountListItem.amount = item.amount;
+            amountListItem.value = index.toString();
+            amountListItem.text = item.name;
+            amountListItem.subtext = "";
+            return amountListItem;
+        });
+    }
+    
+    public render(): React.ReactNode {
+        
+        return (
+            <React.Fragment>
+
+                <TwoColumns>
+
+                    <Form>
+
+                        <Checkbox label="MultiSelect"
+                                  inline
+                                  value={this.state.multiSelect}
+                                  onChange={async (sender, value) => await this.setState({multiSelect:value})}/>
+
+                        <Checkbox label="Search (Uses list of 10 items)"
+                                  inline
+                                  value={this.state.search}
+                                  onChange={async (sender, value) => await this.setSearchAsync(value)}/>
+
+                        <Checkbox label="Favorite"
+                                  inline
+                                  value={this.state.favorite}
+                                  onChange={async (sender, value) => await this.setState({favorite:value})}/>
+                                  
+                        <Checkbox label="Required"
+                                  inline
+                                  value={this.state.required}
+                                  onChange={async (sender, value) => await this.setState({required:value})}/>
+
+                        <Checkbox label="Expanded"
+                                  inline
+                                  value={this.state.expanded}
+                                  onChange={async (sender, value) => await this.setState({expanded:value})}/>
+
+                        <Checkbox label="AmountListItem"
+                                  inline
+                                  value={this.state.amountListItem}
+                                  onChange={async (sender, value) => await this.setState({amountListItem:value})}
+                        />
+                                  
+                    </Form>
+                    
+                    <WidgetContainer>
+
+                        <DropdownWidget id="amountListItems" wide
+                                        label="DropdownWidget"
+                                        items={this.dropdownItems()}
+                                        multiple={this.state.multiSelect}
+                                        favorite={this.state.favorite}
+                                        required={this.state.required}
+                                        expanded={this.state.expanded}
+                                        onChange={async (sender: { id: string; }, item: string, amount: string) => await ch.flyoutMessageAsync(sender.id + ".onChange:" + item + " " + amount)}
+                        />
+                        
+                    </WidgetContainer>
+                    
+                </TwoColumns>
+                
+            </React.Fragment>
+        );
+    }
+}
---
+++ Renta.TestApplication.WebUI/src/pages/Tests/SummaryWidgetTest/SummaryWidgetTest.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import {GeoLocation} from "@weare/athenaeum-toolkit";
+import {BaseComponent} from "@weare/athenaeum-react-common";
+import WidgetContainer from "@/components/WidgetContainer/WidgetContainer";
+import WorkOrderModel from "@/models/server/WorkOrderModel";
+import SummaryWidget from "@/pages/RentaTasks/Summary/SummaryWidget/SummaryWidget";
+import User from "@/models/server/User";
+import CreateAndAssignContactPersonRequest from "@/models/server/requests/CreateAndAssignContactPersonRequest";
+import CreateAndAssignContactPersonResponse from "@/models/server/responses/CreateAndAssignContactPersonResponse";
+import ConstructionSiteOrWarehouse from "@/models/server/ConstructionSiteOrWarehouse";
+
+export interface ISummaryWidgetTestState {
+}
+
+export default class SummaryWidgetTest extends BaseComponent<{}, ISummaryWidgetTestState> {
+    state: ISummaryWidgetTestState = {
+    }
+    private workOrder: WorkOrderModel = new WorkOrderModel();
+
+    private users: User[] = [];
+
+    private fetchApproversAsync(): Promise<User[]> {
+        return new Promise<User[]>((resolve) => resolve(this.users));
+    }
+
+    private async createAndAssignContactPersonAsync(request: CreateAndAssignContactPersonRequest): Promise<CreateAndAssignContactPersonResponse> {
+        
+        const userAlreadyExist: boolean = this.users.some(item => item.email == request.email || item.phone == request.phone);
+        const response = new CreateAndAssignContactPersonResponse();
+
+        if (userAlreadyExist) {
+            response.successfully = false;
+        }
+        else {
+            const contactPerson = new User();
+            contactPerson.id = (10000 * Math.random()).toString();
+            contactPerson.firstname = request.firstname;
+            contactPerson.lastName = request.lastName;
+            contactPerson.middleName = request.middleName;
+            contactPerson.phone = request.phone;
+            contactPerson.email = request.email;
+            response.user = contactPerson;
+            response.successfully = true;
+
+            this.users.push(contactPerson);
+        }
+
+        return new Promise<CreateAndAssignContactPersonResponse>((resolve) => resolve(response));
+    }
+
+    private async onChangeAsync(workOrder: WorkOrderModel): Promise<void> {
+        this.workOrder = workOrder;
+        
+        await this.reRenderAsync();
+    }
+    
+    public async initializeAsync(): Promise<void> {
+        const user1 = new User();
+        user1.id = "1";
+        user1.firstname = "user1";
+        user1.email = "user1@renta.fi";
+        user1.phone = "036452387";
+        user1.isDeleted = false;
+        const user2 = new User();
+        user2.id = "2";
+        user2.firstname = "user2";
+        user2.email = "user2@renta.fi";
+        user2.phone = "036452487";
+        user2.isDeleted = false;
+
+        this.users.push(user1, user2);
+        
+        const owner = new ConstructionSiteOrWarehouse();
+        owner.id = "20f34f";
+        owner.name = "Korjaustyö Porvoon työmaa";
+        
+        let location = new GeoLocation();
+        location.city = "Oulu";
+        location.country = "Finland";
+        location.address = "Aleksanterinkatu 48";
+        location.postalCode = "90120";
+        
+        owner.location = location
+        
+        this.workOrder.name = "TYÖ 1234/22345";
+        this.workOrder.description = "Order from user";
+        this.workOrder.customerOrderer = new User();
+        this.workOrder.customerOrderer.firstname = "User' name";
+        this.workOrder.constructionSiteId = "E0000000-2000-2000-2000-000000000001";
+        this.workOrder.customerApproverId = user1.id;
+        this.workOrder.customerApprover = user1;
+        this.workOrder.completionDate = new Date(2020, 0, 1);
+        this.workOrder.owner = owner;
+        
+    }
+
+    public render(): React.ReactNode {
+
+        return (
+            <React.Fragment>
+
+                <WidgetContainer>
+                    
+                    <SummaryWidget workOrder={this.workOrder}
+                                   fetchApprovers={async () => await this.fetchApproversAsync()}
+                                   createAndAssignContactPerson={async (sender, request) => await this.createAndAssignContactPersonAsync(request)}
+                                   onChange={async (sender, workOrder) => await this.onChangeAsync(workOrder)}
+                    />
+
+                </WidgetContainer>
+                
+            </React.Fragment>
+        );
+    }
+}
