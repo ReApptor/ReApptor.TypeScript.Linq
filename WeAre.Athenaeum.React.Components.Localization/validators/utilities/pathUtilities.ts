026a79256bcb70f847f265afd597544e6cb7be55
Eric Askari <eric.askari@weare.fi>
2021-03-09 13:35:03 +0200

    RSW-5643 Removed validator from components.localization since we have standalone project.

--- WeAre.Athenaeum.React.Components.Localization/validators/utilities/pathUtilities.ts
+++
@@ -1,6 +0,0 @@
-export abstract class PathUtilities {
-  public static componentAbsolutePathToComponentName(dir: string) {
-    const coNameSplit = dir.split('/');
-    return coNameSplit[coNameSplit.length - 1];
-  }
-}
--- WeAre.Athenaeum.React.Components.Localization/validators/utilities/stringUtilities.ts
+++
@@ -1,6 +0,0 @@
-export abstract class StringUtilities {
-    public static pascalToCamelCase(str: string) {
-        const [first, ...rest] = str;
-        return [first.toLowerCase(), ...rest].join('');
-    }
-}
