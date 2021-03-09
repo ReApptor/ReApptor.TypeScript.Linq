518ac966cd8d683c14fa66de16e16bd60529706e
Andrey Popov <andrey.popov@weare.fi>
2021-03-09 14:25:22 +0200

    RSW-450 Title Widget Fix

--- WeAre.Athenaeum.React.Components/src/components/TitleWidget/TitleWidget.tsx
+++ WeAre.Athenaeum.React.Components/src/components/TitleWidget/TitleWidget.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import BaseWidget, { IBaseWidgetProps, IBaseWidgetState } from "../WidgetContainer/BaseWidget";
 import { IconSize, IIconProps } from "../Icon/Icon";
-import {ITypeConverter, TypeConverter} from "@weare/athenaeum-toolkit";
+import {TypeConverter} from "@weare/athenaeum-toolkit";
 import TitleWidgetLocalizer from "@/components/TitleWidget/TitleWidgetLocalizer";
 
 import widgetStyles from "../WidgetContainer/WidgetContainer.module.scss";
@@ -55,12 +55,12 @@ export default class TitleWidget extends BaseWidget<ITitleWidgetProps> {
 
     protected async processDataAsync(state: IBaseWidgetState<any>, data: any | null): Promise<void> {
         if (data != null) {
-            const converter: ITypeConverter | null = TypeConverter.getConverter(data, {} as ITitleModel);
-            //TODO: convert to toTitle
-            const title: ITitleModel = (data || {}) as ITitleModel;
-            state.description = title.description;
-            state.label = title.label;
-            state.icon = title.icon || state.icon;
+            const title: ITitleModel | null = TypeConverter.convert(data, nameof<ITitleModel>());
+            if (title != null) {
+                state.description = title.description;
+                state.label = title.label;
+                state.icon = title.icon || state.icon;
+            }
         }
     }
 
