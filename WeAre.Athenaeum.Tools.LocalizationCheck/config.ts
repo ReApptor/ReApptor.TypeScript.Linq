00855edbb6b15c008f256ea3f6a27459050753c7
Eric Askari <eric.askari@weare.fi>
2021-03-09 13:29:08 +0200

    RSW-5643 Updated config file to work with components.

--- WeAre.Athenaeum.Tools.LocalizationCheck/config.ts
+++ WeAre.Athenaeum.Tools.LocalizationCheck/config.ts
@@ -1,30 +1,12 @@
 import { Configuration, TypescriptLocalizationPrefix } from "./src/types";
 
 const configuration: Configuration = {
+    typescriptLocalizationPrefix: TypescriptLocalizationPrefix.COMPONENT_NAME,
     logSearchStrings: false,
-    resources: ["../../renta-tools/Renta.Tools.WebUI.Resources/SharedResources.resx"],
-    typescriptLocalizationPrefix: TypescriptLocalizationPrefix.LOCALIZER,
-    cSharpDirectories: ["../../renta-tools/Renta.Tools.WebUI", "../../renta-tools/Renta.Tools.WebUI.Server"],
-    typescriptComponentsDirectories: ["../../renta-tools/Renta.Tools.WebUI/src"],
-    enumsToKeep: [
-        "WebApplicationType",
-        "ReportDefinitionType",
-        "DeviceCounterType",
-        "MaintenanceReason",
-        "FaultLevel",
-        "ReportItemType",
-        "ResourceItemType",
-        "LoginResultStatus",
-        "UserRole",
-        "MeasuringOperators",
-        "InvitationType",
-        "AuditTimestamp",
-        "AuthType",
-        "DeviceStatus",
-        "SavePasswordResultStatus",
-        "SortDirection",
-        "InvoiceStatusFilter",
-    ],
+    resources: ["../WeAre.Athenaeum.React.Components.Localization/resources/SharedResources.resx"],
+    cSharpDirectories: [],
+    typescriptComponentsDirectories: ["../WeAre.Athenaeum.React.Components/src/components"],
+    enumsToKeep: [],
 };
 
 export default configuration;
