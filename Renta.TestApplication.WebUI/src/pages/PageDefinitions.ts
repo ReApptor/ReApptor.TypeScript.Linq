6ab3bebaa245c47c67549cfe62b4751355d30ba0
Andrey Popov <andrey.popov@weare.fi>
2021-03-10 10:53:40 +0200

    RSW-450 additional logs are added (temporary);

--- Renta.TestApplication.WebUI/src/pages/PageDefinitions.ts
+++ Renta.TestApplication.WebUI/src/pages/PageDefinitions.ts
@@ -1,8 +1,15 @@
 import {BasePageDefinitions, PageRoute} from "@weare/athenaeum-react-common";
+import {IBasePage} from "@weare/athenaeum-react-common/lib/types/src/base/BasePage";
 
 export default class PageDefinitions extends BasePageDefinitions {
 
+    public createPageAsync(route: PageRoute): Promise<IBasePage> {
+        console.log("PageDefinitions.createPageAsync: route=", route);
+        return super.createPageAsync(route);
+    }
+
     protected async require(pageContainer: string, pageName: string): Promise<any> {
+        console.log("PageDefinitions.require: pageContainer=", pageContainer, "pageName=", pageName);
         //pageName += "Page";
         return await require(`../pages/${pageContainer}${pageName}/${pageName}`);
     }
--- WeAre.Athenaeum.React.Components/src/components/Layout/Layout.tsx
+++ WeAre.Athenaeum.React.Components/src/components/Layout/Layout.tsx
@@ -165,13 +165,22 @@ export default class Layout extends BaseAsyncComponent<ILayoutProps, ILayoutStat
     }
 
     protected async fetchDataAsync(): Promise<ApplicationContext> {
+        console.log("Layout.fetchDataAsync: this.props.fetchContext=", (this.props.fetchContext != null));
         if (this.props.fetchContext) {
             const timezoneOffset: number = Utility.timezoneOffset;
             const applicationType: WebApplicationType = this.getApplicationType();
+            console.log("Layout.fetchDataAsync: timezoneOffset=", timezoneOffset, "applicationType=", applicationType);
             return await this.props.fetchContext(this, timezoneOffset, applicationType);
         }
 
         return super.fetchDataAsync();
+    }
+
+    protected async processDataAsync(state: ILayoutState, data: ApplicationContext | null): Promise<void> {
+        console.log("Layout.processDataAsync: data.currentPage=", data ? data.currentPage : null, " this.state.page=", this.state.page);
+        if ((!this.state.page) && (data) && (data.currentPage)) {
+            await PageRouteProvider.redirectAsync(data.currentPage);
+        }
     }
 
     protected getEndpoint(): string {
@@ -214,6 +223,8 @@ export default class Layout extends BaseAsyncComponent<ILayoutProps, ILayoutStat
     }
 
     public async setPageAsync(page: IBasePage): Promise<void> {
+        
+        console.log("setPageAsync: page=", page);
 
         await this.setState({ page: page });
         
