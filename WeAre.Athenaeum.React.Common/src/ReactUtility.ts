705d802f94c00581ec87500876500f9cd7fe3e0b
Andrey Popov <andrey.popov@weare.fi>
2021-03-11 13:15:52 +0200

    RSW-450 BaseComponent: fix refactoring;

--- WeAre.Athenaeum.React.Common/src/ReactUtility.ts
+++ WeAre.Athenaeum.React.Common/src/ReactUtility.ts
@@ -1,4 +1,4 @@
-import React, {Attributes, ComponentClass, FunctionComponent, ReactChildren, ReactNode} from "react";
+import React from "react";
 import {ReactElement} from "react";
 import {AthenaeumConstants} from "@weare/athenaeum-toolkit";
 
@@ -13,7 +13,7 @@ export default class ReactUtility {
 
         return items
             .map((item: string, index: number) => (index % 2 != 0)
-                ? [this.createElement(tag, {key: `${containerIndex}${tag}${index}`}, item)]
+                ? [React.createElement(tag, {key: `${containerIndex}${tag}${index}`}, item)]
                 : [item]
             )
             .flat();
@@ -51,26 +51,26 @@ export default class ReactUtility {
 
         return lines
             .map((line: string, index: number) => (index < lines.length - 1)
-                ? [...this.containerToMarks(line, index), this.createElement("br", {key: "br" + index})]
+                ? [...this.containerToMarks(line, index), React.createElement("br", {key: "br" + index})]
                 : [...this.containerToMarks(line, index)]
             )
             .flat();
     }
     
-    public static createElement<P extends {}>(type: FunctionComponent<P> | ComponentClass<P> | string, props?: Attributes & P | null, ...children: ReactNode[]): ReactElement<P> {
-        console.log("ReactUtility.createElement: props=", props);
-        const createElement = ((window as any).reactCreateElement) || ((window as any).reactCreateElement = React.createElement);
-        return createElement(type, props, ...children);
-    }
-    
-    public static cloneElement<P>(element: ReactElement<P>, props?: Partial<P> & Attributes, ...children: ReactNode[]): ReactElement<P> {
-        console.log("ReactUtility.cloneElement: element=", element, "props=", props);
-        const cloneElement = ((window as any).reactCloneElement) || ((window as any).reactCloneElement = React.cloneElement);
-        return cloneElement(element, props, ...children);
-    }
-
-    public static get reactChildren(): ReactChildren {
-        console.log("ReactUtility.reactChildren()");
-        return ((window as any).reactChildren || ((window as any).reactChildren = React.Children));
-    }
+    // public static createElement<P extends {}>(type: FunctionComponent<P> | ComponentClass<P> | string, props?: Attributes & P | null, ...children: ReactNode[]): ReactElement<P> {
+    //     console.log("ReactUtility.createElement: props=", props);
+    //     const createElement = ((window as any).reactCreateElement) || ((window as any).reactCreateElement = React.createElement);
+    //     return createElement(type, props, ...children);
+    // }
+    //
+    // public static cloneElement<P>(element: ReactElement<P>, props?: Partial<P> & Attributes, ...children: ReactNode[]): ReactElement<P> {
+    //     console.log("ReactUtility.cloneElement: element=", element, "props=", props);
+    //     const cloneElement = ((window as any).reactCloneElement) || ((window as any).reactCloneElement = React.cloneElement);
+    //     return cloneElement(element, props, ...children);
+    // }
+    //
+    // public static get reactChildren(): ReactChildren {
+    //     console.log("ReactUtility.reactChildren()");
+    //     return ((window as any).reactChildren || ((window as any).reactChildren = React.Children));
+    // }
 }
