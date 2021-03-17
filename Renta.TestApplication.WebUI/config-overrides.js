3f9a9f377c43b621b4a6090b8e07f41d3bd3cd6e
Eric Askari <eric.askari@weare.fi>
2021-03-17 14:35:51 +0200

    RSW-5712 added toolkit as a path to tsconfig and config-overrides.js and working.

--- Renta.TestApplication.WebUI/config-overrides.js
+++ Renta.TestApplication.WebUI/config-overrides.js
@@ -36,6 +36,7 @@ const path = require("path");
 module.exports = override(
     addWebpackAlias({ ["@weare/athenaeum-react-components"]: path.resolve(__dirname, "src/@weare/athenaeum-react-components/index.ts")}),
     addWebpackAlias({ ["@weare/athenaeum-react-common"]: path.resolve(__dirname, "src/@weare/athenaeum-react-common/index.ts")}),
+    addWebpackAlias({ ["@weare/athenaeum-toolkit"]: path.resolve(__dirname, "src/@weare/athenaeum-toolkit/index.ts")}),
     addWebpackAlias({ ["@"]: path.resolve(__dirname, "src")}),
     ...addBabelPlugins("babel-plugin-ts-nameof"),
 );
