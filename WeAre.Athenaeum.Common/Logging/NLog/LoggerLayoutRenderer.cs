d40c4f3c66c524c457dd7b28eeb34dc0e851d0ef
Andrey Popov <andrey.popov@weare.fi>
2021-03-29 12:54:37 +0300

    RSW-450 Logging: "Logger" layout renderer is added (Ping/Pong);

---
+++ WeAre.Athenaeum.Common/Logging/NLog/LoggerLayoutRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using NLog;
+using NLog.LayoutRenderers;
+
+namespace WeAre.Athenaeum.Common.Logging.NLog
+{
+    [LayoutRenderer(Name)]
+    public sealed class LoggerLayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            builder.Append("NLog");
+        }
+
+        /// <summary>
+        /// "logger"
+        /// </summary>
+        public const string Name = "logger";
+    }
+}
