f3a8ce566d11b3e917ede07519952ab1f88bc729
Eric Askari <eric.askari@weare.fi>
2021-03-31 11:46:43 +0300

    RSW-450 added remove on Found to localization check

--- WeAre.Athenaeum.Tools.LocalizationCheck/src/utilities/ResxUtilities.ts
+++ WeAre.Athenaeum.Tools.LocalizationCheck/src/utilities/ResxUtilities.ts
@@ -1,4 +1,5 @@
 import resx, { ObjectOfStrings } from "resx";
+import fs from "fs";
 
 export abstract class ResxUtilities {
     public static async convertResxFileToMap(resxFile: Buffer): Promise<Map<string, string[]>> {
@@ -31,6 +32,38 @@ export abstract class ResxUtilities {
                 resolve(res);
             });
         });
+    }
+
+    public static deleteLocalizationFromResourceFile(resourcePath: string, keyToRemove: string): void {
+        const trimmedKey = keyToRemove.replace('"', '').replace('"', '')
+        
+        const dataTag = `<data name="${trimmedKey}"` 
+        const dataCloseTag = "</data>";
+
+        const resource = fs.readFileSync(resourcePath).toString();
+
+        const startIndex = resource.indexOf(dataTag);
+        const stopIndex = resource.indexOf(dataCloseTag, startIndex) + dataCloseTag.length;
+        
+        if (startIndex === -1) {
+            console.log(`Could not find "${trimmedKey}" in ${resourcePath.substring(resourcePath.lastIndexOf('/') + 1)}`);
+            return;
+        }
+
+
+        console.log("removing: ", trimmedKey);
+
+        const slice = resource.slice(startIndex, stopIndex);
+        let updated = resource.replace(slice, "");
+
+        const nextStartTag = updated.indexOf('<data', startIndex);
+
+        if (nextStartTag !== -1) {
+            console.log('removing empty space');
+            updated = updated.slice(0, startIndex) + updated.slice(nextStartTag)
+        }
+
+        fs.writeFileSync(resourcePath, updated);
     }
 
     public static convertJsToResx() {}
