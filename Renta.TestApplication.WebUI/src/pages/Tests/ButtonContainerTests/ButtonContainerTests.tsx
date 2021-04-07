102d4906f2bd59de664af954a53482815a31a80d
Andrey Popov <andrey.popov@weare.fi>
2021-04-07 18:40:49 +0300

    RSW-450 CL: Button: new prop "right" to align to the right in ButtonContainer;

--- Renta.TestApplication.WebUI/src/pages/Tests/ButtonContainerTests/ButtonContainerTests.tsx
+++ Renta.TestApplication.WebUI/src/pages/Tests/ButtonContainerTests/ButtonContainerTests.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {BaseComponent, ch, Justify} from "@weare/athenaeum-react-common";
-import {Button, ButtonType, Form, TwoColumns, ButtonContainer, FourColumns} from "@weare/athenaeum-react-components";
+import {BaseComponent} from "@weare/athenaeum-react-common";
+import {Button, ButtonType, ButtonContainer} from "@weare/athenaeum-react-components";
 
 interface IButtonTestsState {
 }
