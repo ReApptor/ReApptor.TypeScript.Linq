b553654849621a4f2ade2837b82750777f1241d9
Andrey Popov <andrey.popov@weare.fi>
2021-04-07 18:40:29 +0300

    RSW-450 CL: Button: new prop "right" to align to the right in ButtonContainer;

---
+++ Renta.TestApplication.WebUI/src/pages/Tests/ButtonContainerTests/ButtonContainerTests.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {BaseComponent, ch, Justify} from "@weare/athenaeum-react-common";
+import {Button, ButtonType, Form, TwoColumns, ButtonContainer, FourColumns} from "@weare/athenaeum-react-components";
+
+interface IButtonTestsState {
+}
+
+export default class ButtonContainerTests extends BaseComponent<{}, IButtonTestsState> {
+
+    state: IButtonTestsState = {
+    };
+
+    public render(): React.ReactNode {
+
+        return (
+            <React.Fragment>
+
+                <ButtonContainer className={"mt-3"}>
+
+                    <Button right label={"Orange"} type={ButtonType.Orange}/>
+
+                    <Button right label={"Orange (icon)"} icon={{name: "fal plus"}} type={ButtonType.Orange}/>
+
+                </ButtonContainer>
+
+                <ButtonContainer className={"mt-3"}>
+
+                    <Button label={"Orange"} type={ButtonType.Orange}/>
+
+                    <Button label={"Orange (icon)"} icon={{name: "fal plus"}} type={ButtonType.Orange}/>
+
+                </ButtonContainer>
+
+            </React.Fragment>
+        );
+    }
+}
