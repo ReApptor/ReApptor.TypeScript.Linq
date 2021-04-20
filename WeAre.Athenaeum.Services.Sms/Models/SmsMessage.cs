cdb57613cfec7d2c113098c1db8c9c5dc74f0073
Juhani Sihvonen <juhani.sihvonen@weare.fi>
2021-04-20 15:04:38 +0300

    RSW-450 sending sms using AwsPinpoint

---
+++ WeAre.Athenaeum.Services.Sms/Models/SmsMessage.cs
@@ -0,0 +1,34 @@
+namespace WeAre.Athenaeum.Services.Sms.Models
+{
+    public class SmsMessage
+    {
+        #region Constructors
+
+        public SmsMessage()
+        {
+        }
+
+        public SmsMessage(string receiver, string message)
+        {
+            Receiver = receiver;
+            Message = message;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// receiver of the message
+        /// </summary>
+        public string Receiver { get; set; }
+        
+
+        /// <summary>
+        /// The message to send
+        /// </summary>
+        public string Message { get; set; }
+
+        #endregion
+    }
+}
