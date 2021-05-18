57e8d8a0e03809f03c93fe772f487881a5a88ede
Andrey Popov <andrey.popov@weare.fi>
2021-05-18 18:10:46 +0300

    RSW-450 "BaseSsoSettings" has been created;

--- Renta.Apps.Common/Configuration/Settings/AzureSsoSettings.cs
+++ Renta.Apps.Common/Configuration/Settings/AzureSsoSettings.cs
@@ -1,20 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 
- namespace Renta.Apps.Common.Configuration.Settings
+namespace Renta.Apps.Common.Configuration.Settings
 {
-    public sealed class AzureSsoSettings : IOptions<AzureSsoSettings>
+    public sealed class AzureSsoSettings : BaseSsoSettings, IOptions<AzureSsoSettings>
     {
         AzureSsoSettings IOptions<AzureSsoSettings>.Value => this;
 
         public string ApplicationId { get; set; }
 
         public string ClientSecret { get; set; }
-
-        public CookieSecurePolicy SecurePolicy { get; set; } = CookieSecurePolicy.Always;
-
-        public SameSiteMode SameSite { get; set; } = SameSiteMode.Lax;
-
-        public int ExpirationTimeoutInSec { get; set; } = 600;
     }
 }
