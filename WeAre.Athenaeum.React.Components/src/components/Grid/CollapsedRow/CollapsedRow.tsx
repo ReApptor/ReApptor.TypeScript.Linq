513fcb61467485912cac96c4afa6202e4f2bf1f2
Andrey Popov <andrey.popov@weare.fi>
2021-06-06 12:49:37 +0300

    RSW-450 CL: GRID: Collapsed rows;

--- WeAre.Athenaeum.React.Components/src/components/Grid/CollapsedRow/CollapsedRow.tsx
+++ WeAre.Athenaeum.React.Components/src/components/Grid/CollapsedRow/CollapsedRow.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {BaseComponent} from "@weare/athenaeum-react-common";
 import Cell from "../Cell/Cell";
-import Icon, {IconSize} from "../../Icon/Icon";
 import {CellModel, GridModel, RowModel} from "../GridModel";
 import GridLocalizer from "../GridLocalizer";
 
@@ -15,12 +14,6 @@ interface ICollapsedRowProps<TItem = {}> {
 }
 
 export default class CollapsedRow<TItem = {}> extends BaseComponent<ICollapsedRowProps<TItem>> {
-    
-    private async toggleAsync(row: RowModel<TItem>): Promise<void> {
-        row.collapsed = !row.collapsed;
-        
-        await this.reRenderAsync();
-    }
 
     public get grid(): GridModel<TItem> {
         return this.props.grid;
@@ -44,43 +37,27 @@ export default class CollapsedRow<TItem = {}> extends BaseComponent<ICollapsedRo
             ? row.cells.where(item => item.column.collapsed)
             : [];
 
-        if (collapsedCells.length == 0) {
+        if ((collapsedCells.length == 0) || (!row.responsiveContainerExpanded)) {
             return (<React.Fragment/>);
         }
 
-        const primaryCollapsedCell: CellModel<TItem> = cells[0];
-        const minWidth: number = primaryCollapsedCell.column.outerWidth();
-        const expanded: boolean = !row.collapsed;
-        const iconName: string = (expanded) ? "fas minus-circle" : "fas plus-circle";
-
         return (
             <tr className={styles.collapsedRow}>
-                <td colSpan={cells.length}>
+                <td colSpan={cells.length + 1}>
                     <table>
-                        <tr className={styles.primary}>
-                            <td onClick={() => this.toggleAsync(row)} style={{minWidth: minWidth}}>
-                                <Icon name={iconName} size={IconSize.Large} />
-                            </td>
-                            <Cell key={primaryCollapsedCell.key}
-                                  cell={primaryCollapsedCell}
-                            />
-                        </tr>
                         {
-                            (expanded) &&
-                            (
-                                collapsedCells.map((cell: CellModel<TItem>) => {
-                                    return (
-                                        <tr>
-                                            <td>
-                                                <span>{this.toMultiLines(GridLocalizer.get(cell.column.header)) || this.props.children}</span>
-                                            </td>
-                                            <Cell key={cell.key}
-                                                  cell={cell}
-                                            />
-                                        </tr>
-                                    )
-                                })
-                            )
+                            collapsedCells.map((cell: CellModel<TItem>) => {
+                                return (
+                                    <tr>
+                                        <td>
+                                            <span>{this.toMultiLines(GridLocalizer.get(cell.column.header)) || this.props.children}</span>
+                                        </td>
+                                        <Cell key={cell.key}
+                                              cell={cell}
+                                        />
+                                    </tr>
+                                )
+                            })
                         }
                     </table>
                 </td>
