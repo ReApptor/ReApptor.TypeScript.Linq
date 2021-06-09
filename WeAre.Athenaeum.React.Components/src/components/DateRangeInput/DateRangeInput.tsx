a848a99a4dbcb4bc6c4531e9a2469c580979ca7d
Eric Askari <eric.askari@weare.fi>
2021-06-09 17:21:01 +0300

    RSW-6474 DateRangeInput coding

--- WeAre.Athenaeum.React.Components/src/components/DateRangeInput/DateRangeInput.tsx
+++ WeAre.Athenaeum.React.Components/src/components/DateRangeInput/DateRangeInput.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {IBaseInputProps} from "../BaseInput/BaseInput";
 
 import styles from "./DateRangeInput.module.scss";
 import Icon, {IconSize} from "../Icon/Icon";
 import {BaseComponent} from "@weare/athenaeum-react-common";
 import DateRangeInputLocalizer from "./DateRangeInputLocalizer";
+import {Utility} from "@weare/athenaeum-toolkit";
 
 enum WeekDaysEnum {
     Sunday,
@@ -16,7 +16,12 @@ enum WeekDaysEnum {
     Saturday
 }
 
-interface IDateRangeInputProps extends IBaseInputProps<Date> {
+interface IDateRangeInputProps {
+    className?: string;
+    sameDay?: boolean;
+    startValue?: Date;
+    endValue?: Date;
+    onValueChange?: (start: Date, end: Date) => Promise<void>
 }
 
 interface IDateRangeInputState {
@@ -36,16 +41,16 @@ const LONG_MONTH_GRID = 42;
 export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, IDateRangeInputState> {
     private activeMonthView: Date = this.defaultActiveMonthView;
     private gridDays: GridDay[] = this.defaultGridDays;
-    
+
     state: IDateRangeInputState = {
         lastHoveredGrid: null,
-        firstClickedGrid: null,
-        lastClickedGrid: null
+        firstClickedGrid: this.defaultFirstClicked,
+        lastClickedGrid: this.defaultEndClicked
     };
 
     private get defaultActiveMonthView(): Date {
-        if (this.props.value) {
-            return this.props.value
+        if (this.props.startValue) {
+            return this.props.startValue
         }
 
         return new Date();
@@ -53,6 +58,26 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
 
     private get defaultGridDays(): GridDay[] {
         return this.getGridDays(this.activeMonthView.getFullYear(), this.activeMonthView.getMonth());
+    }
+
+    private get defaultFirstClicked(): GridDay | null {
+        if (!this.props.startValue) {
+            return null
+        }
+
+        return {
+            unixTime: DateRangeInput.startOfDayInUnix(this.props.startValue.getFullYear(), this.props.startValue.getMonth(), this.props.startValue.getDate())
+        }
+    }
+
+    private get defaultEndClicked(): GridDay | null {
+        if (!this.props.endValue) {
+            return null
+        }
+        
+        return {
+            unixTime: DateRangeInput.startOfDayInUnix(this.props.endValue.getFullYear(), this.props.endValue.getMonth(), this.props.endValue.getDate())
+        }
     }
 
     private getGridDays(year: number, month: number): GridDay[] {
@@ -85,6 +110,7 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
         
         const introMonthGridDays: GridDay[] = new Array(introDaysCount).fill(0).map((val: 0, index: number): GridDay => {
             const day = lastDayOfPreviousMonth - index;
+            
             return  {
                 unixTime: DateRangeInput.startOfDayInUnix(year, previousMonth, day)
 
@@ -93,6 +119,7 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
 
         const outroMonthGridDays: GridDay[] = new Array(outroDaysCount).fill(0).map((val: 0, index: number): GridDay => {
             const day = firstDayOfNextMonth + index;
+            
             return  {
                 unixTime: DateRangeInput.startOfDayInUnix(year, nextMonth, day)
             }
@@ -102,10 +129,20 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
     }
 
     private async onDayGridClick(gridDay: GridDay): Promise<void> {
+
+        if (this.state.firstClickedGrid === gridDay && !this.state.lastClickedGrid && this.props.sameDay) {
+            this.state.lastClickedGrid = gridDay;
+            await this.reRenderAsync();
+            await this.emitOutput();
+            return;
+        }
+        
         if (this.state.firstClickedGrid === gridDay) {
+            
             this.state.firstClickedGrid = null;
             this.state.lastClickedGrid = null;
             await this.reRenderAsync();
+            await this.emitOutput();
             return;
         }
         
@@ -113,6 +150,7 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
             this.state.firstClickedGrid = null;
             this.state.lastClickedGrid = null;            
             await this.reRenderAsync();
+            await this.emitOutput();
             return;
         }
         
@@ -120,18 +158,21 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
             this.state.firstClickedGrid = gridDay;
             this.state.lastClickedGrid = null;
             await this.reRenderAsync();
+            await this.emitOutput();
             return; 
         }
         
         if (this.state.firstClickedGrid && !this.state.lastClickedGrid) {
             this.state.lastClickedGrid = gridDay;
             await this.reRenderAsync();
+            await this.emitOutput();
             return;
         }
 
         this.state.firstClickedGrid = gridDay;
         this.state.lastClickedGrid = null;
         await this.reRenderAsync();
+        await this.emitOutput();
     }
     
     private async onDayGridMouseEnter(gridDay: GridDay): Promise<void> {
@@ -149,6 +190,22 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
         this.activeMonthView = new Date(this.activeMonthView.setMonth(this.activeMonthView.getMonth() - 1));
         this.gridDays = this.getGridDays(this.activeMonthView.getFullYear(), this.activeMonthView.getMonth());
         await this.reRenderAsync();
+    }
+
+    private async emitOutput() {
+        if (!this.props.onValueChange || !this.state.firstClickedGrid || !this.state.lastClickedGrid) {
+            return;
+        }
+        
+        const start = new Date(this.state.firstClickedGrid.unixTime);
+        const end = new Date(this.state.lastClickedGrid.unixTime);
+
+        if (start.getTime() > end.getTime()) {
+            await this.props.onValueChange(end, start);
+            return;
+        }
+        
+        await this.props.onValueChange(start, end);
     }
 
     private isDayGridSelected(gridDay: GridDay): boolean {
@@ -178,40 +235,64 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
         return  false
     }
 
+    private renderWeekDays(): JSX.Element[] {
+        const weekDays = [
+            WeekDaysEnum.Monday, 
+            WeekDaysEnum.TuesDay, 
+            WeekDaysEnum.Wednesday, 
+            WeekDaysEnum.Thursday, 
+            WeekDaysEnum.Friday, 
+            WeekDaysEnum.Saturday, 
+            WeekDaysEnum.Sunday
+        ];
+        
+        
+        return weekDays.map(weekDay => <div className={styles.weekDayName}>{Utility.getShortDayOfWeek(weekDay)}</div>)
+    }
+    
     private renderGridDays(gridDay: GridDay): JSX.Element {
         const isSelected = this.isDayGridSelected(gridDay) && styles.isSelected || "";
+
+        const isToday = DateRangeInput.todayInUnixTime() === gridDay.unixTime ? styles.isToday : "";
         
-        const isInRange = this.isDayGridInRange(gridDay) && styles.isInRange || "";
+        const isInRangeAndSelected = (this.state.firstClickedGrid && this.state.lastClickedGrid) && this.isDayGridInRange(gridDay) ? styles.isInRangeAndSelected : "";
         
-        const className: string = this.css(styles.monthViewGridDay, isInRange, isSelected);
+        const isInRangeAndNotSelected = (this.state.firstClickedGrid && !this.state.lastClickedGrid) && this.isDayGridInRange(gridDay) ? styles.isInRangeAndNotSelected : "";
+        
+        const className: string = this.css(styles.monthViewGridDay, isInRangeAndSelected, isInRangeAndNotSelected, isSelected, isToday);
         
         const onClick: () => void = async () => await this.onDayGridClick(gridDay);
         
         const onMouseEnter: () => void = async () => await this.onDayGridMouseEnter(gridDay);
         
-        return <div className={className} onMouseEnter={onMouseEnter} onClick={onClick}>{DateRangeInput.getDayOfUnixTime(gridDay.unixTime)}</div>
+        return <div className={className} onMouseEnter={onMouseEnter} onClick={onClick}><span>{DateRangeInput.getDayOfMonth(gridDay.unixTime)}</span></div>
     }
 
     public render(): React.ReactNode {
+        const monthName: string = new Intl.DateTimeFormat(DateRangeInputLocalizer.language, {month: 'long'}).format(this.activeMonthView);
+        const year: number = this.activeMonthView.getFullYear();
+        
         return (
             <div className={this.css(styles.dateRangeInput, this.props.className)}>
 
-                <div className={this.css(styles.dateRangeInputMonthView)}>
+                <span className={styles.monthName}>{monthName} {year}</span>
 
-                    <div className={styles.monthAction}  onClick={() => this.onPreviousMonthClick()}>
-                        <Icon name="caret-left" size={IconSize.Large}/>
-                    </div>
-
-                    <span className={styles.month}>{new Intl.DateTimeFormat(DateRangeInputLocalizer.language, { month: 'long'}).format(this.activeMonthView)}</span>
-
-                    <div className={styles.monthAction} onClick={() => this.onNextMonthClick()}>
-                        <Icon name="caret-right" size={IconSize.Large} />
-                    </div>
-
-                    {
-                        this.gridDays.map(gridDay => this.renderGridDays(gridDay))
-                    }
+                <div className={styles.monthAction} onClick={() => this.onPreviousMonthClick()}>
+                    <Icon name="chevron-up" size={IconSize.Normal}/>
                 </div>
+
+
+                <div className={styles.monthAction} onClick={() => this.onNextMonthClick()}>
+                    <Icon name="chevron-down" size={IconSize.Normal}/>
+                </div>
+
+                {
+                    this.renderWeekDays()
+                }
+
+                {
+                    this.gridDays.map(gridDay => this.renderGridDays(gridDay))
+                }
             </div>
         );
     }
@@ -228,7 +309,12 @@ export default class DateRangeInput extends BaseComponent<IDateRangeInputProps, 
         return new Date(year, month, day, 0, 0, 0, 0).getTime();
     }
 
-    private static getDayOfUnixTime (unixTime: number): number {
+    private static getDayOfMonth (unixTime: number): number {
         return new Date(unixTime).getDate();
+    }
+
+    private static todayInUnixTime(): number {
+        const now = new Date();
+        return DateRangeInput.startOfDayInUnix(now.getFullYear(), now.getMonth(), now.getDate())
     }
 }
