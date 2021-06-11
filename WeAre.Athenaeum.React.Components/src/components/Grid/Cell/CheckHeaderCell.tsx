d02cab2eab7d80f63297ed2474b63db07d718662
Andrey Popov <andrey.popov@weare.fi>
2021-06-11 16:29:55 +0300

    RSW-450 CL: Grid fix;

--- WeAre.Athenaeum.React.Components/src/components/Grid/Cell/CheckHeaderCell.tsx
+++ WeAre.Athenaeum.React.Components/src/components/Grid/Cell/CheckHeaderCell.tsx
@@ -7,10 +7,11 @@ import gridStyles from "../Grid.module.scss";
 interface ICheckHeaderCellProps<TItem = {}> {
     model: GridModel<TItem>;
     colSpanLeft: boolean;
+    hasHeaderGroups: boolean;
 }
 
 export default class CheckHeaderCell<TItem = {}> extends BaseComponent<ICheckHeaderCellProps<TItem>> {
-    
+
     public get model(): GridModel<TItem> {
         return this.props.model;
     }
@@ -29,25 +30,26 @@ export default class CheckHeaderCell<TItem = {}> extends BaseComponent<ICheckHea
         model.checked = newChecked;
 
         await model.reRenderAsync();
-        
+
         if (model.onCheck) {
             await model.onCheck(model);
         }
     }
 
-    render(): React.ReactNode {
+    public render(): React.ReactNode {
 
         const model: GridModel<TItem> = this.model;
-        
+
         model.checkHeaderInstance = this;
 
         const checked: boolean = (model.checked !== false);
         const partiallyStyle: any = (model.checked === undefined) && gridStyles.partially;
 
-        const colSpan: number = this.props.colSpanLeft ? 2 : 1;
+        const rowSpan: number | undefined = (this.props.hasHeaderGroups) ? 2 : undefined;
+        const colSpan: number | undefined = (this.props.colSpanLeft) ? 2 : undefined;
 
         return (
-            <th rowSpan={2} colSpan={colSpan}
+            <th rowSpan={rowSpan} colSpan={colSpan}
                 className={this.css(gridStyles.check, partiallyStyle)}
                 onClick={async () => this.onCheckAsync()}>
                 
