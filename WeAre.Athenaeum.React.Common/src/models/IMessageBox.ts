55c50b198b708da2c81a0044a09bdc6d0f543063
Andrey Popov <andrey.popov@weare.fi>
2021-07-28 13:12:08 +0300

    RSW-450 CL: "MessageBox" component;

---
+++ WeAre.Athenaeum.React.Common/src/models/IMessageBox.ts
@@ -0,0 +1,23 @@
+import {MessageBoxButtons, MessageBoxIcon} from "../Enums";
+
+export type MessageBoxModelCallback = () => string | IMessageBox;
+
+export interface IMessageBoxButtons {
+    okButton?: string | boolean;
+    cancelButton?: string | boolean;
+    abortButton?: string | boolean;
+    retryButton?: string | boolean;
+    ignoreButton?: string | boolean;
+    yesButton?: string | boolean;
+    noButton?: string | boolean;
+}
+
+export interface IMessageBox extends IMessageBoxButtons {
+    title: string;
+    caption?: string;
+    icon?: MessageBoxIcon | string;
+    buttons?: MessageBoxButtons;
+    placeholder?: string;
+    minLength?: number;
+    comment?: boolean;
+}
