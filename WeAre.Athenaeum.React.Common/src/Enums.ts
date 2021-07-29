4dd0a93ee3deae8e2aaa04a04c252b5bda645d1b
Andrey Popov <andrey.popov@weare.fi>
2021-07-29 12:58:40 +0300

    RSW-450 CL: "MessageBox" component (comments);

--- WeAre.Athenaeum.React.Common/src/Enums.ts
+++ WeAre.Athenaeum.React.Common/src/Enums.ts
@@ -58,45 +58,44 @@ export enum AlertType {
  * Specifies identifiers to indicate the return value of a dialog box.
  */
 export enum DialogResult {
-    //
-    // Summary:
-    //     Nothing is returned from the dialog box. This means that the modal dialog continues
-    //     running.
+    /**
+     * Nothing is returned from the dialog box. This means that the modal dialog continues running.
+     */
     None = 0,
 
-    //
-    // Summary:
-    //     The dialog box return value is OK (usually sent from a button labeled OK).
+    /**
+     * The dialog box return value is OK (usually sent from a button labeled OK).
+     */
     OK = 1,
 
-    //
-    // Summary:
-    //     The dialog box return value is Cancel (usually sent from a button labeled Cancel).
+    /**
+     * The dialog box return value is Cancel (usually sent from a button labeled Cancel).
+     */
     Cancel = 2,
 
-    //
-    // Summary:
-    //     The dialog box return value is Abort (usually sent from a button labeled Abort).
+    /**
+     * The dialog box return value is Abort (usually sent from a button labeled Abort).
+     */
     Abort = 3,
 
-    //
-    // Summary:
-    //     The dialog box return value is Retry (usually sent from a button labeled Retry).
+    /**
+     * The dialog box return value is Retry (usually sent from a button labeled Retry).
+     */
     Retry = 4,
 
-    //
-    // Summary:
-    //     The dialog box return value is Ignore (usually sent from a button labeled Ignore).
+    /**
+     * The dialog box return value is Ignore (usually sent from a button labeled Ignore).
+     */
     Ignore = 5,
 
-    //
-    // Summary:
-    //     The dialog box return value is Yes (usually sent from a button labeled Yes).
+    /**
+     * The dialog box return value is Yes (usually sent from a button labeled Yes).
+     */
     Yes = 6,
 
-    //
-    // Summary:
-    //     The dialog box return value is No (usually sent from a button labeled No).
+    /**
+     * The dialog box return value is No (usually sent from a button labeled No).
+     */
     No = 7
 }
 
@@ -104,28 +103,34 @@ export enum DialogResult {
  * Specifies constants defining which buttons to display on a System.Windows.Forms.MessageBox.
  */
 export enum MessageBoxButtons {
-    //
-    //     The message box contains an OK button.
+    /**
+     * The message box contains an OK button.
+     */
     OK,
 
-    //
-    //     The message box contains OK and Cancel buttons.
+    /**
+     * The message box contains OK and Cancel buttons.
+     */
     OKCancel,
 
-    //
-    //     The message box contains Abort, Retry, and Ignore buttons.
+    /**
+     * The message box contains Abort, Retry, and Ignore buttons.
+     */
     AbortRetryIgnore,
 
-    //
-    //     The message box contains Yes, No, and Cancel buttons.
+    /**
+     * The message box contains Yes, No, and Cancel buttons.
+     */
     YesNoCancel,
 
-    //
-    //     The message box contains Yes and No buttons.
+    /**
+     * The message box contains Yes and No buttons.
+     */
     YesNo,
 
-    //
-    //     The message box contains Retry and Cancel buttons.
+    /**
+     * The message box contains Retry and Cancel buttons.
+     */
     RetryCancel,
 
     Custom = 6,
@@ -135,49 +140,55 @@ export enum MessageBoxButtons {
  * Specifies constants defining which information to display.
  */
 export enum MessageBoxIcon {
-    //
-    //     The message box contain no symbols.
-    //
+    /**
+     * The message box contain no symbols.
+     */
     None = 0,
 
-    //
-    //     The message box contains a symbol consisting of a white X in a circle with a red background.
+    /**
+     * The message box contains a symbol consisting of a white X in a circle with a red background.
+     */
     Hand = 16,
 
-    //
-    //     The message box contains a symbol consisting of white X in a circle with a red background.
+    /**
+     * The message box contains a symbol consisting of white X in a circle with a red background.
+     */
     Stop = 16,
 
-    //
-    //     The message box contains a symbol consisting of white X in a circle with a red background.
+    /**
+     * The message box contains a symbol consisting of white X in a circle with a red background.
+     */
     Error = 16,
 
-    //
-    //     The message box contains a symbol consisting of a question mark in a circle.
-    //     The question-mark message icon is no longer recommended because it does not clearly
-    //     represent a specific type of message and because the phrasing of a message as
-    //     a question could apply to any message type. In addition, users can confuse the
-    //     message symbol question mark with Help information. Therefore, do not use this
-    //     question mark message symbol in your message boxes. The system continues to support
-    //     its inclusion only for backward compatibility.
+    /**
+     * The message box contains a symbol consisting of a question mark in a circle.
+      * The question-mark message icon is no longer recommended because it does not clearly
+      * represent a specific type of message and because the phrasing of a message as
+      * a question could apply to any message type. In addition, users can confuse the
+      * message symbol question mark with Help information. Therefore, do not use this
+      * question mark message symbol in your message boxes. The system continues to support
+      * its inclusion only for backward compatibility.
+     */
     Question = 32,
 
-    //
-    //     The message box contains a symbol consisting of an exclamation point in a triangle
-    //     with a yellow background.
+    /**
+     * The message box contains a symbol consisting of an exclamation point in a triangle with a yellow background.
+     */
     Exclamation = 48,
 
-    //
-    //     The message box contains a symbol consisting of an exclamation point in a triangle
-    //     with a yellow background.
+    /**
+     * The message box contains a symbol consisting of an exclamation point in a triangle with a yellow background.
+     */
     Warning = 48,
 
-    //
-    //     The message box contains a symbol consisting of a lowercase letter i in a circle.
+    /**
+     * The message box contains a symbol consisting of a lowercase letter i in a circle.
+     */
     Asterisk = 64,
 
-    //
-    //     The message box contains a symbol consisting of a lowercase letter i in a circle.
+    /**
+     * The message box contains a symbol consisting of a lowercase letter i in a circle.
+     */
     Information = 64
 }
 
