0fc68fdff84fab84070f867779401ccaa83be60b
Andrey Popov <andrey.popov@weare.fi>
2021-07-30 17:48:39 +0300

    RSW-450 Renta configuration: AMC support;

--- Renta.Apps.Common/Configuration/RentaConfiguration.cs
+++ Renta.Apps.Common/Configuration/RentaConfiguration.cs
@@ -1,12 +1,13 @@
 using Microsoft.Extensions.Hosting;
 using WeAre.Athenaeum.Common.Configuration;
+using WeAre.Athenaeum.Common.Interfaces.ACM;
 
 namespace Renta.Apps.Common.Configuration
 {
     public abstract class RentaConfiguration<TConfiguration> : BaseEnvironmentConfiguration<TConfiguration> where TConfiguration : RentaConfiguration<TConfiguration>
     {
-        protected RentaConfiguration(IHostEnvironment environment)
-            : base(environment)
+        protected RentaConfiguration(IHostEnvironment environment, ICredentialService credentialService = null)
+            : base(environment, credentialService)
         {
         }
     }
