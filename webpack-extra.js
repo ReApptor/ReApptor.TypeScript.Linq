47410bc1075032bdbf79639a921514a53564a499
Eric Askari <eric.askari@weare.fi>
2021-08-03 01:55:45 +0300

    RSW-6730 Root files update

---
+++ webpack-extra.js
@@ -0,0 +1,22 @@
+const original = require('@nrwl/react/plugins/webpack.js');
+
+function getWebpackConfig(config) {
+  config.module.rules.push(
+    {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }
+      ]
+    }
+  );
+
+  return original(config);
+}
+
+module.exports = getWebpackConfig;
