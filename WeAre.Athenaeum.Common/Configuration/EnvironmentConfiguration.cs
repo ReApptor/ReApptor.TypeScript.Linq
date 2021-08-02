8857208a43f9face0d913adb7002d6514b43c139
oleksandrnefodov <oleksandr.nefodov@weare.fi>
2021-08-02 17:38:54 +0300

    logs added

--- WeAre.Athenaeum.Common/Configuration/EnvironmentConfiguration.cs
+++ WeAre.Athenaeum.Common/Configuration/EnvironmentConfiguration.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WeAre.Athenaeum.Common.Configuration
 {
     public class EnvironmentConfiguration : BaseEnvironmentConfiguration<EnvironmentConfiguration>
     {
-        public EnvironmentConfiguration(IWebHostEnvironment environment)
-            : base(environment)
+        public EnvironmentConfiguration(IWebHostEnvironment environment, ILogger<EnvironmentConfiguration> logger)
+            : base(environment, logger)
         {
         }
     }
