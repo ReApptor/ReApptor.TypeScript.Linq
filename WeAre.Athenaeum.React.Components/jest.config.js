43c3ec5cdd753c1e36402e7c8ea82b0f9cfc5932
Eric Askari <eric.askari@weare.fi>
2021-08-03 02:03:53 +0300

    RSW-6730 copied all configuration files

--- WeAre.Athenaeum.React.Components/jest.config.js
+++ WeAre.Athenaeum.React.Components/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  displayName: 'WeAre.Athenaeum.React.Toolkit',
+  displayName: 'WeAre.Athenaeum.React.Components',
   preset: '../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../coverage/libs/WeAre.Athenaeum.React.Toolkit',
+  coverageDirectory: '../coverage/libs/WeAre.Athenaeum.React.Components',
 };
