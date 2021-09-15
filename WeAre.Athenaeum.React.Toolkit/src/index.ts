d2404732cd086be69ce36f9d3b191543fedf42c9
Jani Kauppinen <jani.kauppinen@weare.fi>
2021-09-15 12:03:13 +0300

    RSW-450 fix React.Toolkit barrel-file exports

--- WeAre.Athenaeum.React.Toolkit/src/index.ts
+++ WeAre.Athenaeum.React.Toolkit/src/index.ts
@@ -5,6 +5,13 @@ export * from "./models/GeoLocation";
 export * from "./models/TimeSpan";
 export * from "./models/IPagedList";
 export *  from "./models/ISelectListItem";
+export {default as FileModel} from "./models/FileModel";
+export {default as GeoCoordinate} from "./models/GeoCoordinate";
+export {default as GeoLocation} from "./models/GeoLocation";
+export {default as TimeSpan} from "./models/TimeSpan";
+export {default as IPagedList} from "./models/IPagedList";
+export {default as ISelectListItem}  from "./models/ISelectListItem";
+
 //Utilities:
 export * from "./ArrayUtility";
 export * from "./NumberUtility";
@@ -13,33 +20,6 @@ export * from "./BoolUtility";
 export * from "./DateUtility";
 export * from "./Utility";
 export * from "./HashCodeUtility";
-//Extensions:
-export * from "./extensions/DateExtensions";
-export * from "./extensions/StringExtensions";
-export * from "./extensions/ArrayExtensions";
-export * from "./extensions/NumberExtensions";
-//Providers:
-export * from "./providers/BaseTransformProvider";
-export * from "./providers/BaseEnumProvider";
-export * from "./providers/TypeResolver";
-export * from "./providers/ServiceProvider";
-export * from "./providers/TypeConverter";
-export * from "./providers/StringConverter";
-//Helpers:
-export * from "./helpers/PwaHelper";
-export {default} from "./helpers/Asserter/Assert"
-//Other:
-export * from "./localization/BaseLocalizer";
-export *  from "./AthenaeumConstants";
-
-//Models:
-export {default as FileModel} from "./models/FileModel";
-export {default as GeoCoordinate} from "./models/GeoCoordinate";
-export {default as GeoLocation} from "./models/GeoLocation";
-export {default as TimeSpan} from "./models/TimeSpan";
-export {default as IPagedList} from "./models/IPagedList";
-export {default as ISelectListItem}  from "./models/ISelectListItem";
-//Utilities:
 export {default as ArrayUtility} from "./ArrayUtility";
 export {default as NumberUtility} from "./NumberUtility";
 export {default as StringUtility} from "./StringUtility";
@@ -47,15 +27,34 @@ export {default as BoolUtility} from "./BoolUtility";
 export {default as DateUtility} from "./DateUtility";
 export {default as Utility} from "./Utility";
 export {default as HashCodeUtility} from "./HashCodeUtility";
+
+//Extensions:
+export * from "./extensions/DateExtensions";
+export * from "./extensions/StringExtensions";
+export * from "./extensions/ArrayExtensions";
+export * from "./extensions/NumberExtensions";
+
 //Providers:
+export * from "./providers/BaseTransformProvider";
+export * from "./providers/BaseEnumProvider";
+export * from "./providers/TypeResolver";
+export * from "./providers/ServiceProvider";
+export * from "./providers/TypeConverter";
+export * from "./providers/StringConverter";
 export {default as BaseTransformProvider} from "./providers/BaseTransformProvider";
 export {default as BaseEnumProvider} from "./providers/BaseEnumProvider";
 export {default as TypeResolver} from "./providers/TypeResolver";
 export {default as ServiceProvider} from "./providers/ServiceProvider";
 export {default as TypeConverter} from "./providers/TypeConverter";
 export {default as StringConverter} from "./providers/StringConverter";
+
 //Helpers:
+export * from "./helpers/PwaHelper";
 export {default as PwaHelper} from "./helpers/PwaHelper";
+export {default as assert} from "./helpers/Asserter/Assert"
+
 //Other:
+export * from "./localization/BaseLocalizer";
+export *  from "./AthenaeumConstants";
 export {default as BaseLocalizer} from "./localization/BaseLocalizer";
 export {default as AthenaeumConstants}  from "./AthenaeumConstants";
