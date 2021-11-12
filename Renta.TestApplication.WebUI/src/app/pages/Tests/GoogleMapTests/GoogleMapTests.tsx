8c39097e280b3156808c5dc58c4596b490af1864
Jani Kauppinen <jani.kauppinen@weare.fi>
2021-11-12 12:26:45 +0200

    RSW-450 add generic Google map component

---
+++ Renta.TestApplication.WebUI/src/app/pages/Tests/GoogleMapTests/GoogleMapTests.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {BaseComponent, ch, DocumentPreviewSize} from "@weare/athenaeum-react-common";
+import {GoogleMap} from "@weare/athenaeum-react-components";
+
+interface IModalTestsState {
+    documentPreviewSize: DocumentPreviewSize;
+}
+
+export default class GoogleMapTests extends BaseComponent<{}, IModalTestsState> {
+
+    state: IModalTestsState = {
+        documentPreviewSize: DocumentPreviewSize.Medium
+    };
+
+    public render(): React.ReactNode {
+
+        return (
+            <React.Fragment>
+                <GoogleMap height={500}
+                           initialCenter={{lat: 50, lng: 50}}
+                           initialZoom={5}
+                />
+            </React.Fragment>
+        );
+    }
+}
