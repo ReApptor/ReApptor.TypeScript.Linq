71c4aafafa834c691e695721bea66e52e0d89a91
Juhani Sihvonen <juhani.sihvonen@weare.fi>
2021-11-23 10:28:27 +0200

    RSW-450 improve routing

--- Renta.TestApplication.WebUI/src/app/pages/PageDefinitions.ts
+++ Renta.TestApplication.WebUI/src/app/pages/PageDefinitions.ts
@@ -5,30 +5,26 @@ import Tests2 from "./Tests2/Tests2";
 
 export default class PageDefinitions extends BasePageDefinitions {
 
-    
+
     protected async require(pageContainer: string, pageName: string): Promise<any> {
         return await require(`./${pageContainer}${pageName}/${pageName}`);
     }
 
     // eslint-disable-next-line
     constructor() {
-        let pageRoutes: Map<string, PageRoute> = new Map<string, PageRoute>()
-        pageRoutes.set("test2", PageDefinitions.testsRoute2)
+        super();
+    }
 
-        super(pageRoutes);
+    public getRoutes(): Dictionary<string, PageRoute> {
+        let pageRoutes: Dictionary<string, PageRoute> = new Dictionary<string, PageRoute>()
+        pageRoutes.setValue("test2", PageDefinitions.testsRoute2)
+
+        return pageRoutes;
     }
 
     public static readonly testsRoute2: PageRoute = new PageRoute(nameof(Tests2));
 
     public static readonly testsRoute: PageRoute = new PageRoute(nameof(Tests));
-    
-    
-    // public static override getRoutes(): Map<string, PageRoute> | null | any {
-    //     let routes: Map<string, PageRoute> = new Map<string, PageRoute>()
-    //     routes.set("jep", PageDefinitions.testsRoute)
-    //
-    //     return routes;
-    // }
 
 }
 
