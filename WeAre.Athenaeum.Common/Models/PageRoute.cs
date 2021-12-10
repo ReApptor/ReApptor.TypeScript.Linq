8ef0938458964e287803a47a8525b926c15c95e2
Andrey Popov <andrey.popov@weare.fi>
2021-12-10 10:04:00 +0200

    RSW-450 PageRoute is moved to CL;

---
+++ WeAre.Athenaeum.Common/Models/PageRoute.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+
+namespace WeAre.Athenaeum.Common.Models
+{
+    [DebuggerDisplay("{Name}")]
+    public class PageRoute
+    {
+        public string Name { get; set; }
+        
+        public int? Index { get; set; }
+        
+        public string Id { get; set; }
+        
+        public object Parameters { get; set; }
+        
+        public bool IsPageRoute => true;
+
+        public PageRoute()
+        {
+        }
+
+        public PageRoute(string name, int? index = null, string id = null, object parameters = null)
+        {
+            Name = name;
+            Index = index;
+            Id = id;
+            Parameters = parameters;
+        }
+
+        public static implicit operator PageRoute(string name)
+        {
+            return new PageRoute { Name = name };
+        }
+    }
+}
