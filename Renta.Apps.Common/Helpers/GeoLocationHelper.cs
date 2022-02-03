f85d916fd74b646939469019a0567363effdd199
Andrey Popov <andrey.popov@weare.fi>
2022-02-03 12:15:50 +0200

    RSW-450 Additional formatted address parser has been added, unit tests are added, formatted address includes LAT/LON;

--- Renta.Apps.Common/Helpers/GeoLocationHelper.cs
+++ Renta.Apps.Common/Helpers/GeoLocationHelper.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Renta.Apps.Common.Interfaces.Geo;
 using Renta.Apps.Common.Models.Geo;
 using WeAre.Athenaeum.Common.Helpers;
+using WeAre.Athenaeum.Toolkit;
 
 namespace Renta.Apps.Common.Helpers
 {
@@ -87,7 +90,80 @@ namespace Renta.Apps.Common.Helpers
             }
         }
 
-        public static string GetFormattedAddress(this IGeoAddress address)
+        public static IGeoLocation ReadFormattedAddress(string formattedAddress)
+        {
+            if (string.IsNullOrWhiteSpace(formattedAddress))
+            {
+                return null;
+            }
+            
+            IGeoLocation geoLocation = new GeoLocation
+            {
+                Address = formattedAddress,
+                Country = null,
+                PostalBox = null,
+                City = null,
+                PostalCode = null,
+            };
+                
+            //Formatted address comes in formats:
+            //  "Äyritie 12, 01510 Vantaa, Finland, #60.21083359999999, 24.951597"
+            //  "Äyritie 12, Vantaa, Finland, #60.21083359999999, 24.951597"
+            //  "Äyritie 12, Vantaa, 01510, Finland, #60.21083359999999, 24.951597"
+            //  "Äyritie 12, Vantaa, Finland, #60.21083359999999, 24.951597"
+
+            bool includesLocation = (formattedAddress.Contains("#", StringComparison.InvariantCultureIgnoreCase));
+            if (includesLocation)
+            {
+                string[] parts = formattedAddress
+                    .Replace("#", "")
+                    .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(item => item.Trim())
+                    .Where(item => !string.IsNullOrWhiteSpace(item))
+                    .ToArray();
+
+                if (parts.Length > 4)
+                {
+                    string lat = parts[^2];
+                    string lon = parts[^1];
+
+                    string[] cityPostalCodeParts = parts[^4].Trim().Split(" ");
+                    if (cityPostalCodeParts.Length == 2)
+                    {
+                        geoLocation.PostalCode = cityPostalCodeParts[0];
+                        geoLocation.City = cityPostalCodeParts[1];
+                    } else if (cityPostalCodeParts.Length == 1)
+                    {
+                        if (parts.Length > 5)
+                        {
+                            geoLocation.PostalCode = parts[^4];
+                            geoLocation.City = parts[^5];
+                        }
+                        else
+                        {
+                            geoLocation.City = cityPostalCodeParts[0];
+                        }
+                    }
+                        
+                    if ((Utility.TryToDecimal(lat, out decimal latitude)) && (latitude > 0))
+                    {
+                        geoLocation.Lat = latitude;
+                    }
+
+                    if ((Utility.TryToDecimal(lon, out decimal longitude)) && (longitude > 0))
+                    {
+                        geoLocation.Lon = longitude;
+                    }
+                    
+                    geoLocation.Address = parts[0];
+                    geoLocation.Country = parts[^3].GetCountryName();
+                }
+            }
+
+            return geoLocation;
+        }
+
+        public static string GetFormattedAddress(this IGeoAddress address, bool includeCoordinates = false)
         {
             if (address != null)
             {
@@ -97,7 +173,14 @@ namespace Renta.Apps.Common.Helpers
                 
                 if (items.Length > 0)
                 {
-                    return string.Join(", ", items);
+                    string formattedAddress = string.Join(", ", items);
+                    
+                    if ((includeCoordinates) && (address is IGeoCoordinate coordinates) && (coordinates.Lat > 0) && (coordinates.Lon > 0))
+                    {
+                        //#LAT, LON"
+                        var formatInfo = new NumberFormatInfo { NumberDecimalSeparator = "." };
+                        return $"{formattedAddress}, #{coordinates.Lat.ToString(formatInfo)}, {coordinates.Lon.ToString(formatInfo)}";
+                    }
                 }
             }
 
