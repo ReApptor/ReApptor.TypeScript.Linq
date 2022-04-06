d0a76f06ed20257aad1b80ccda6bfb2d26b8c858
Andrey Popov <andrey.popov@weare.fi>
2022-04-06 13:10:45 +0300

    CL: NumberInput new prop "hideZero";

--- WeAre.ReApptor.React.Components/src/components/NumberInput/NumberInput.tsx
+++ WeAre.ReApptor.React.Components/src/components/NumberInput/NumberInput.tsx
@@ -29,7 +29,9 @@ export interface INumberInputProps extends IBaseInputProps<number> {
     hideArrows?: boolean;
     increaseIcon?: string | IIconProps;
     decreaseIcon?: string | IIconProps;
+    hideZero?: boolean;
     onChange?(sender: NumberInput, value: number, userInteraction: boolean, done: boolean): Promise<void>;
+    onFocus?(sender: NumberInput): Promise<void>;
     onBlur?(sender: NumberInput): Promise<void>;
 }
 
@@ -40,6 +42,7 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
 
     private _ref: React.RefObject<HTMLInputElement> | null = null;
     private _acceptableStr: string | null = null;
+    private _focused: boolean = false;
 
     private async onChangeAsync(e: React.FormEvent<HTMLInputElement>): Promise<void> {
         const str: string = e.currentTarget.value;
@@ -148,9 +151,12 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
     }
 
     private getStr(): string {
-        return (this._acceptableStr !== null)
-            ? this._acceptableStr
-            : this.str;
+        const hideZero: boolean = (this.hideZero) && (!this.focused) && (this.value == 0);
+        return (hideZero)
+            ? ""
+            : (this._acceptableStr !== null)
+                ? this._acceptableStr
+                : this.str;
     }
 
     private get ref(): React.RefObject<HTMLInputElement> {
@@ -162,7 +168,17 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
         return numberFormat.format;
     }
 
+    protected async valueFocusHandlerAsync(): Promise<void> {
+        this._focused = true;
+
+        if (this.props.onFocus) {
+            await this.props.onFocus(this);
+        }
+    }
+
     protected async valueBlurHandlerAsync(): Promise<void> {
+        this._focused = false;
+        
         await this.saveChangesAsync();
 
         await super.valueBlurHandlerAsync();
@@ -201,6 +217,10 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
         return (this.props.clickToEdit)
             ? NumberInputBehaviour.Restricted
             : this.props.behaviour || NumberInputBehaviour.ValidationOnChange;
+    }
+
+    public get focused(): boolean {
+        return this._focused;
     }
 
     public get value(): number {
@@ -253,6 +273,10 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
 
         return decreaseIconProp;
     }
+    
+    public get hideZero(): boolean {
+        return (this.props.hideZero === true);
+    }
 
     protected get allowFloat(): boolean {
         return (this.step < 1.0);
@@ -298,8 +322,9 @@ export default class NumberInput extends BaseInput<number, INumberInputProps, IN
                        className="form-control"
                        title={this.props.title}
                        placeholder={this.props.placeholder}
-                       onChange={async (e: React.FormEvent<HTMLInputElement>) => await this.onChangeAsync(e)}
-                       onBlur={async () => await this.valueBlurHandlerAsync()}
+                       onChange={(e: React.FormEvent<HTMLInputElement>) => this.onChangeAsync(e)}
+                       onFocus={() => this.valueFocusHandlerAsync()}
+                       onBlur={() => this.valueBlurHandlerAsync()}
                        onKeyDown={async (e: React.KeyboardEvent<HTMLInputElement>) => await this.onInputKeyDownHandlerAsync(e)}
                 />
 
