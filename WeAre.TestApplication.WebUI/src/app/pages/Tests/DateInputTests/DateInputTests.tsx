6f3d75e3dd3bf6c149fa11f38b1fd2bcc4bb34e3
Andrey Popov <andrey.popov@weare.fi>
2022-04-20 09:43:10 +0300

    CL: rollback BaseInput;

--- WeAre.TestApplication.WebUI/src/app/pages/Tests/DateInputTests/DateInputTests.tsx
+++ WeAre.TestApplication.WebUI/src/app/pages/Tests/DateInputTests/DateInputTests.tsx
@@ -46,84 +46,52 @@ export default class DateInputTests extends BaseComponent {
     
     private readonly _ref: React.RefObject<DateInput> = React.createRef();
     
-    public render(): React.ReactNode {
+    public render(): React.ReactNode {        
         return (
             <React.Fragment>
 
                 <Form className="pt-4">
 
-                    <Checkbox inline className="pt-1 pb-1" label="todayButton" value={this.state.todayButton} onChange={async (sender, todayButton) => {
-                        await this.setState({todayButton})
-                    }}/>
+                    <Checkbox inline className="pt-1 pb-1" label="todayButton" value={this.state.todayButton} onChange={async (sender, todayButton) => {await this.setState({todayButton})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="inline" value={this.state.inline} onChange={async (sender, inline) => {await this.setState({inline})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="popup" value={this.state.popup} onChange={async (sender, popup) => {await this.setState({popup})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="showTime" value={this.state.showTime} onChange={async (sender, showTime) => {await this.setState({showTime})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="showOnlyTime" value={this.state.showOnlyTime} onChange={async (sender, showOnlyTime) => {await this.setState({showOnlyTime})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="small" value={this.state.small} onChange={async (sender, small) => {await this.setState({small})}} />
 
-                    <Checkbox inline className="pt-1 pb-1" label="inline" value={this.state.inline} onChange={async (sender, inline) => {
-                        await this.setState({inline})
-                    }}/>
+                    <Checkbox inline className="pt-1 pb-1" label="showYearDropdown" value={this.state.showYearDropdown} onChange={async (sender, showYearDropdown) => {await this.setState({showYearDropdown})}} />
 
-                    <Checkbox inline className="pt-1 pb-1" label="popup" value={this.state.popup} onChange={async (sender, popup) => {
-                        await this.setState({popup})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="showTime" value={this.state.showTime} onChange={async (sender, showTime) => {
-                        await this.setState({showTime})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="showOnlyTime" value={this.state.showOnlyTime} onChange={async (sender, showOnlyTime) => {
-                        await this.setState({showOnlyTime})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="small" value={this.state.small} onChange={async (sender, small) => {
-                        await this.setState({small})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="showYearDropdown" value={this.state.showYearDropdown} onChange={async (sender, showYearDropdown) => {
-                        await this.setState({showYearDropdown})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="showMonthDropdown" value={this.state.showMonthDropdown} onChange={async (sender, showMonthDropdown) => {
-                        await this.setState({showMonthDropdown})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="showMonthYearPicker" value={this.state.showMonthYearPicker} onChange={async (sender, showMonthYearPicker) => {
-                        await this.setState({showMonthYearPicker})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="readonly" value={this.state.readonly} onChange={async (sender, readonly) => {
-                        await this.setState({readonly})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="rentaStyle" value={this.state.rentaStyle} onChange={async (sender, rentaStyle) => {
-                        await this.setState({rentaStyle})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="expanded" value={this.state.expanded} onChange={async (sender, expanded) => {
-                        await this.setState({expanded})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="minDate" value={this.state.minDateActive} onChange={async (sender, minDateActive) => {
-                        await this.setState({minDateActive})
-                    }}/>
-
-                    <Checkbox inline className="pt-1 pb-1" label="maxDate" value={this.state.maxDateActive} onChange={async (sender, maxDateActive) => {
-                        await this.setState({maxDateActive})
-                    }}/>
-
+                    <Checkbox inline className="pt-1 pb-1" label="showMonthDropdown" value={this.state.showMonthDropdown} onChange={async (sender, showMonthDropdown) => {await this.setState({showMonthDropdown})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="showMonthYearPicker" value={this.state.showMonthYearPicker} onChange={async (sender, showMonthYearPicker) => {await this.setState({showMonthYearPicker})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="readonly" value={this.state.readonly} onChange={async (sender, readonly) => {await this.setState({readonly})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="rentaStyle" value={this.state.rentaStyle} onChange={async (sender, rentaStyle) => {await this.setState({rentaStyle})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="expanded" value={this.state.expanded} onChange={async (sender, expanded) => {await this.setState({expanded})}} />
+                             
+                    <Checkbox inline className="pt-1 pb-1" label="minDate" value={this.state.minDateActive} onChange={async (sender, minDateActive) => {await this.setState({minDateActive})}} />
+                    
+                    <Checkbox inline className="pt-1 pb-1" label="maxDate" value={this.state.maxDateActive} onChange={async (sender, maxDateActive) => {await this.setState({maxDateActive})}} />
+                    
                     {
-                        this.state.minDateActive && <DateInput className="pt-1 pb-1" value={this.state.minDate || undefined} inline label="minDate" onChange={async (minDate) => {
-                            await this.setState({minDate})
-                        }}/>
+                        this.state.minDateActive && <DateInput className="pt-1 pb-1" value={this.state.minDate || undefined} inline label="minDate" onChange={async (minDate)=>{ await this.setState({minDate})}}/>
                     }
-
+                       
                     {
-                        this.state.maxDateActive && <DateInput className="pt-1 pb-1" value={this.state.maxDate || undefined} inline label="maxDate" onChange={async (maxDate) => {
-                            await this.setState({maxDate})
-                        }}/>
+                        this.state.maxDateActive && <DateInput className="pt-1 pb-1" value={this.state.maxDate || undefined}  inline label="maxDate" onChange={async (maxDate)=>{ await this.setState({maxDate})}}/>
                     }
-
+                    
                     <OneColumn>
 
                         <DateInput ref={this._ref}
-                                   label="Start"
+                                   label="Start" 
                                    todayButton={this.state.todayButton ? "Today" : ""}
                                    inline={this.state.inline}
                                    popup={this.state.popup}
@@ -139,7 +107,7 @@ export default class DateInputTests extends BaseComponent {
                                    value={this.state.normalDateInputValue}
                                    minDate={this.state.minDateActive ? this.state.minDate : null}
                                    maxDate={this.state.maxDateActive ? this.state.maxDate : null}
-                                   onChange={async (normalDateInputValue) => { await this.setState({normalDateInputValue}) }}/>
+                                   onChange={async (normalDateInputValue)=>{ await this.setState({normalDateInputValue})}} />
                         
                     </OneColumn>
 
