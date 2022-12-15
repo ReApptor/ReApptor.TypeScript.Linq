77a48e23cf6ea5d3835e989b6127094eb306f56f
oleksandrnefodov <oleksandr.nefodov@weare.fi>
2022-12-15 13:55:27 +0200

    CL: Jest tests;

--- ReApptor.TypeScript.Linq/src/tests/ArrayUtility.count.spec.ts
+++ ReApptor.TypeScript.Linq/src/tests/ArrayUtility.count.spec.ts
@@ -3,7 +3,6 @@ import {ArrayExtensions} from "../index";
 ArrayExtensions();
 
 describe("count", function() {
-    
     test("count0-from0", function () {
         const result: number = [].count();
         expect(result).toEqual(0);
