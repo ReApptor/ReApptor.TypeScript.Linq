6f5e51a23f97f201efd50a3b9109b407dd34af96
Andrey Popov <andrey.popov@weare.fi>
2022-12-16 15:32:15 +0200

    CL: ReApptor.TypeScript.Linq: static "init" methods to init array extensions;

--- ReApptor.TypeScript.Linq/src/index.ts
+++ ReApptor.TypeScript.Linq/src/index.ts
@@ -1,10 +1,7 @@
-// Interfaces:
-
-// Models:
-
 // Utilities:
 export * from "./utilities/ArrayUtility";
 export {default as ArrayUtility} from "./utilities/ArrayUtility";
 
 // Extensions:
 export * from "./extensions/ArrayExtensions";
+export {default as Linq} from "./extensions/ArrayExtensions";
--- ReApptor.TypeScript.Linq/src/tests/ArrayUtility.all.spec.ts
+++ ReApptor.TypeScript.Linq/src/tests/ArrayUtility.all.spec.ts
@@ -1,6 +1,6 @@
-import {ArrayExtensions} from "../index";
+import {Linq} from "../index";
 
-ArrayExtensions();
+Linq.init();
 
 describe("all", function() {
     test("all-true-from0", function () {
