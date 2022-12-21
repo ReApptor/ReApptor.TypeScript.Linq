39d763d4d2f50cdaf7e1136f9f9416aa7bedb01a
Andrey Popov <andrey.popov@weare.fi>
2022-12-21 11:26:52 +0200

    CL: ToolbarContainer expanded to implement "IBaseContainerComponentProps"

--- WeAre.ReApptor.React.Components/src/components/ToolbarContainer/ToolbarContainer.tsx
+++ WeAre.ReApptor.React.Components/src/components/ToolbarContainer/ToolbarContainer.tsx
@@ -1,17 +1,16 @@
 import React from "react";
-import {BaseComponent} from "@weare/reapptor-react-common";
+import {BaseComponent, IBaseContainerComponentProps} from "@weare/reapptor-react-common";
 
 import styles from "./ToolbarContainer.module.scss";
 
-interface IToolbarContainerProps {
-    className?: string;
+interface IToolbarContainerProps extends IBaseContainerComponentProps {
 }
 
 export default class ToolbarContainer extends BaseComponent<IToolbarContainerProps> {
-    
-    render(): React.ReactNode {
-        return(
-            <div className={this.css(styles.toolbarContainer, "d-flex justify-content-between", this.props.className)}>
+
+    public render(): React.ReactNode {
+        return (
+            <div id={this.id} className={this.css(styles.toolbarContainer, "d-flex justify-content-between", this.props.className)}>
                 {this.children}
             </div>
         );
