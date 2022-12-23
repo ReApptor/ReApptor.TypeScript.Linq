77761d1ab0a0a6c8920725083c067640312fad99
Andrey Popov <61983961+YW5kcmV5@users.noreply.github.com>
2022-12-23 15:51:29 +0200

    .gitignore

--- dist/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import "./extensions/ArrayExtensions";
-export * from "./utilities/ArrayUtility";
-export { default as ArrayUtility } from "./utilities/ArrayUtility";
-export * from "./extensions/ArrayExtensions";
-export default class Linq {
-    static init(): void;
-}
